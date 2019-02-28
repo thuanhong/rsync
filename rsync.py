02cd732c86d66d33799803dee514ecacc97264ad
HongThuan <guest@localhost.localdomain>
2019-02-28 08:00:11 -0500

    add

--- rsync.py
+++ rsync.py
@@ -3,26 +3,40 @@
 import os
 import argparse
 
+def check_exist(dest):
+    return os.path.exists(dest)
 
-def regularWrite(src, dest):
-    if os.path.islink(src):
-        os.symlink(os.readlink(src), dest)
-    #  open source file and destination file
-    f_src = os.open(src, os.O_RDONLY)
-    if os.path.isdir(args.dest):
-        dest  = dest + '/' + src
-    f_dest = os.open(dest, os.O_CREAT | os.O_WRONLY)
+def check_size(src, dest):
+    return os.stat(src).st_size == os.stat(src).st_size
 
-    f_src_stat = os.stat(src)
-    content = os.read(f_src, f_src_stat.st_size)
-    os.write(f_dest, content)
-    os.close(f_src)
-    os.close(f_dest)
-    #  set default time and permission
-    os.utime(dest, (f_src_stat.st_atime, f_src_stat.st_mtime))
-    os.chmod(dest, f_src_stat.st_mode)
-    #  set default symlink and hardlink
+def check_time(src, dest):
+    return os.stat(src).st_mtime == os.stat(dest).st_mtime
 
+def check_hardlink(src):
+    return os.stat(src).st_nlink > 1
+
+def check_symlink(src):
+    return os.path.islink(src)
+
+def check_update(src, dest):
+    time_src = os.stat(src).st_mtime
+    time_dest = os.stat(src).st_mtime
+    return time_src < time_dest
+
+
+def regularWrite(source, destination):
+    f_source = os.open(source, os.O_RDONLY)
+    f_destination = os.open(destination, os.O_CREAT | os.O_WRONLY)
+    content = os.read(f_source, os.stat(source).st_size)
+    os.write(f_destination, content)
+    os.close(f_source)
+    os.close(f_destination)
+
+
+def set_default(source, destination):
+    f_source_stat = os.stat(source)
+    os.utime(destination, (f_source_stat.st_atime, f_source_stat.st_mtime))
+    os.chmod(destination, f_source_stat.st_mode)
 
 
 if __name__ == '__main__':
@@ -30,11 +44,38 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("-c", "--checksum", help = "skip based on checksum, not mod-time & size", action="store_true")
     parser.add_argument("-u", "--update", help = 'skip files that are newer on the receiver', action="store_true")
-    parser.add_argument("src", help = "file src")
-    parser.add_argument("dest", help = 'file dest')
+    parser.add_argument("-r", "--recursive", help = "copy multifile", action="store_true")
+    parser.add_argument("source", nargs="+", help = "file source")
+    parser.add_argument("destination", help = 'file destination')
     args = parser.parse_args()
+    #  create directory if it not exist
+    if args.recursive and not check_exist(args.destination):
+        os.mkdir(args.destination)
 
-    if os.path.isfile(args.src):
-        regularWrite(args.src, args.dest)
-    else:
-        print('skipping directory new')
+    for element in args.source:
+        #  check sym link and hard link
+        if not os.path.exists(element):
+             print('rsync: link_stat "' + os.path.abspath(element) + '" failed: No such file or directory(2)')
+             break
+        elif not os.access(element, os.R_OK):
+            print('rsync: send_files failed to open ' + os.path.abspath(element) + ': Permission denied (13)')
+            break
+        elif check_symlink(element) or check_hardlink(element):
+            #  delete file if it have exist
+            if check_exist(args.destination) and not os.path.isdir(args.destination):
+                os.unlink(args.destination)
+
+            if check_symlink(element):
+                os.symlink(os.readlink(element), args.destination)
+            else:
+                os.link(element, args.destination + '/' + element)
+        elif os.path.isdir(args.destination):
+            regularWrite(element, args.destination)
+            set_default(element, args.destination)
+        elif check_exist(args.destination):
+            if (check_time(element, args.destination) and check_size(element, args.destination))\
+                or (args.update and check_update(element, args.destination)):
+                pass
+        else:
+            regularWrite(element, args.destination)
+            set_default(element, args.destination)
